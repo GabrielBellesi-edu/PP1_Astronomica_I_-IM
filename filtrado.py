844973b479f782579447ca2fbc9c77ed67da7b87
GabrielBellesi-edu <gabriel.bellesi.edu@gmail.com>
2025-05-27 15:00:25 -0300

    Update filtrado.py a filtrado_ambig.py
    Se renombro la funcion, ya que creamos una segunda funcion de filtrado. 
    Agregando funcionalidad de borrar las colunas ambig irex y file
    Renomnbrado de funciones.
    Se juntaron funciones para optimizar rendimiento 
    Modifico rutas de archivos

--- filtrado.py
+++ filtrado.py
@@ -1,26 +1,33 @@
 import pandas as pd
 from pathlib import Path
 
-def filtrar_csv_por_columna(ruta_entrada="Test/mpd2024_csv", ruta_salida="Test/mpd2024_csv", columna_objetivo=9, valor_permitido=1):
+def filtro_ambig(ruta_entrada="INDICE_DE_MASA/csv/conversion",
+           ruta_salida="INDICE_DE_MASA/csv/filtrado/ambig", 
+           columna_objetivo=9,
+           valor_permitido=1,
+           columnas_a_eliminar=[2, 9, 12]):
+    
     ruta_entrada = Path(ruta_entrada)
     ruta_salida = Path(ruta_salida)
     ruta_salida.mkdir(exist_ok=True)
-    
+
     archivos_csv = sorted(ruta_entrada.glob("*.csv"))
 
     for archivo in archivos_csv:
-        print(f"Filtrando: {archivo.name}");
-        print ("hola chicos");
-        # Leer el CSV sin encabezado, ya que no lo tiene
+        print(f"Procesando: {archivo.name}")
+
+        # Leer el CSV sin encabezado
         df = pd.read_csv(archivo, sep=";", header=None, decimal=",")
 
-        # Filtrar solo filas donde la columna 10 (índice 9) sea igual al valor permitido (por defecto: 1)
+        # Filtrar por ambigüedad
         df_filtrado = df[df[columna_objetivo] == valor_permitido]
 
-        # Guardar el archivo filtrado
+        # Eliminar columnas especificadas
+        df_resultado = df_filtrado.drop(columns=columnas_a_eliminar)
+
+        # Guardar resultado
         nuevo_nombre = ruta_salida / archivo.name
-        df_filtrado.to_csv(nuevo_nombre, index=False, sep=";", decimal=",", header=False)
+        df_resultado.to_csv(nuevo_nombre, index=False, sep=";", decimal=",", header=False)
 
-    print("✅ Filtrado completado correctamente.")
-
-filtrar_csv_por_columna()
+    print("✅ Proceso de filtrado y depuración completado correctamente.")
+filtro_ambig()
